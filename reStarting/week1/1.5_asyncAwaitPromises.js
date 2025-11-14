ee0203da7924c4277c02c5027afdbd7f2b03523f
tHBP0007 <theHalfBloodPrince0007@gmail.com>
2025-11-14 21:46:20 +0530

    added some part of code in 1.5 file

--- reStarting/week1/1.5_asyncAwaitPromises.js
+++ reStarting/week1/1.5_asyncAwaitPromises.js
@@ -1,3 +1,17 @@
+//context switching: pani garam ho rha h, tabtak mai veggies cut kr rha hu
+//but see you can only cut vegies or take out maggie from the packets (no context switching)
+
+//cd .\reStarting\week1\ --> node .\1.5_asyncAwaitPromises.js --> else it will not read from a.txt
+// const fs = require("fs");
+
+// fs.readFile("a.txt", "utf-8", function (err, data) {
+//     if (err) {
+//         console.log("Something went wrong", err);
+//     } else {
+//         console.log(data);
+//     }
+// })
+
 /**************Wraper func(callBack Hell)********************************************/
 // const fs = require("fs");
 

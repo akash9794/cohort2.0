e0b8ee008a4dfb223d7b17647d47eab2d27b9f76
tHBP0007 <theHalfBloodPrince0007@gmail.com>
2025-11-14 22:55:33 +0530

    cpmpleted 2.1

--- reStarting/week2/2.1_revisionAsyncAwait.js
+++ reStarting/week2/2.1_revisionAsyncAwait.js
@@ -19,3 +19,44 @@
 // console.log("Hii");
 
 
+/************callBack Function*************/
+function greet(name, callback) {
+  console.log("Hello " + name);
+  callback();   // calling the callback
+}
+
+function sayBye() {
+  console.log("Goodbye!");
+}
+
+greet("Akash", sayBye);
+
+
+/*****promises*****************************/
+let promise = new Promise((resolve, reject) => {
+  let success = true;
+
+  if (success) resolve("Work done!");
+  else reject("Something went wrong");
+});
+
+promise
+  .then(result => console.log(result)) 
+  .catch(error => console.log(error));
+
+  
+/****************async await**********/
+function work() {
+  return new Promise(resolve => {
+    resolve("Work finished!");
+  });
+}
+
+async function start() {
+  let result = await work();
+  console.log(result);
+}
+
+start();
+
+
